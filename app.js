b88ea762f8ca89cef02c7066cf128bda5c954404
George Embry <georgetembry@yahoo.com>
2017-05-15 15:45:38 -0700

    added getRandomInt

---
+++ app.js
@@ -0,0 +1,41 @@
+'use strict'
+
+var pike = {
+  minCust:23,
+  maxCust:3,
+  AvgSale:6.3
+}
+
+var seatac = {
+  minCust:3,
+  maxCust:24,
+  AvgSale:1.2
+}
+var Scenter = {
+  minCust:11,
+  maxCust:38,
+  AvgSale:3.7
+}
+var capHill = {
+  minCust:2,
+  maxCust:38,
+  AvgSale:2.3
+}
+var Alki = {
+  minCust:0,
+  maxCust:16,
+  AvgSale:4.6
+}
+
+// declares function that give a random number between min and max customers
+function getRandomInt(minCust, maxCust) {
+  minCust = Math.ceil(min);
+  maxCust = Math.floor(max);
+  return Math.floor(Math.random() * (maxCust - minCust)) + minCust;
+}
+
+// Declare function that gets # cookies sold per hour for each store. Use AvgSale and getRandomInt
+
+function CookiesPerHour {
+
+}
