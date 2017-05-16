c8e4997872e52d9874b9e1ffed612131a858a2d6
George Embry <georgetembry@yahoo.com>
2017-05-15 18:52:02 -0700

    created object returns random number

--- app.js
+++ app.js
@@ -1,41 +1,54 @@
-'use strict'
+'use strict';
 
 var pike = {
+  name: '1st and Pike',
   minCust:23,
   maxCust:3,
-  AvgSale:6.3
-}
+  avgSale:6.3
+};
 
 var seatac = {
+  name: 'Seatac Airport',
   minCust:3,
   maxCust:24,
-  AvgSale:1.2
-}
-var Scenter = {
+  avgSale:1.2
+};
+var center = {
+  name: 'Seattle Center',
   minCust:11,
   maxCust:38,
-  AvgSale:3.7
-}
+  avgSale:3.7
+};
 var capHill = {
+  name: 'Capitol Hill',
   minCust:2,
   maxCust:38,
-  AvgSale:2.3
-}
-var Alki = {
+  avgSale:2.3
+};
+var alki = {
+  name: 'Alki',
   minCust:0,
   maxCust:16,
-  AvgSale:4.6
-}
+  avgSale:4.6,
+  randomNumGen: function (minCust, maxCust)
+  {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+  }
+};
+alki.randomNumGen() * alki.avgSale;
+  //   var getCookies = function() {
+  //
+  // }
 
-// declares function that give a random number between min and max customers
-function getRandomInt(minCust, maxCust) {
-  minCust = Math.ceil(min);
-  maxCust = Math.floor(max);
-  return Math.floor(Math.random() * (maxCust - minCust)) + minCust;
-}
+// Declare function that gives a random number between min and max customers. Multiply RandomCustNum and average cookies per sale.
 
-// Declare function that gets # cookies sold per hour for each store. Use AvgSale and getRandomInt
 
-function CookiesPerHour {
-
-}
+// function storeSales(store) {
+//   var list = document.createElement('ul');
+//   var listArr = [];
+//   var timeArr = ['0600', '0700', '0800', '0900', '1000', '1100', '1200', '1300', '1400', '1500', '1600', '1700', '1800', '1900', '2000'];
+//
+//   for (var i=0; i < 15; i++) {
+//     var numCookies = CookiesSoldHr(store.minCust, store.maxCust, store.avgSale);
+//   }
+// }
