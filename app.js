d2fa26dcb4df9c0bfc834032537486845d02967a
George Embry <georgetembry@yahoo.com>
2017-05-16 18:27:01 -0700

    initial day 2 commit

--- app.js
+++ app.js
@@ -1,52 +1,42 @@
-'use strict';
+'use strict'
 
-var pike = {
-  name: '1st and Pike',
-  minCust:23,
-  maxCust:3,
-  avgSale:6.3
-};
+function Store(name, minCust, maxCust, avgSale) {
+  this.name = name; // dynamic name property
+  this.minCust = minCust; // dynamic minCust property
+  this.maxCust = maxCust; // dynamic maxCust property
+  this.avgSale = avgSale; // dynamic avgSale property
+}
 
-var seatac = {
-  name: 'Seatac Airport',
-  minCust:3,
-  maxCust:24,
-  avgSale:1.2
-};
-var center = {
-  name: 'Seattle Center',
-  minCust:11,
-  maxCust:38,
-  avgSale:3.7
-};
-var capHill = {
-  name: 'Capitol Hill',
-  minCust:2,
-  maxCust:38,
-  avgSale:2.3
-};
-var alki = {
-  name: 'Alki',
-  minCust:0,
-  maxCust:16,
-  avgSale:4.6,
-  randomNumGen: function (minCust, maxCust)
-  {
-    return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
-  }
-};
-alki.randomNumGen() * alki.avgSale;
-  //   var getCookies = function() {
-  //
-  // }
+var pike = new Store('1st and Pike', 23, 3, 6.3);
+var seatac = new Store('Seatac Airport', 3, 24, 1.2);
+var sCenter = new Store('Seattle Center', 11, 38, 3.7);
+var capHill = new Store('Capitol Hill', 20, 38, 2.3);
+var alki = new Store('Alki', 2, 16, 4.6);
+
+// var stores = [pike, seatac, sCenter, capHill, alki];
+
+var table = document.getElementbyId('shell');
+var data = [];
+
+// function that generates random number of cookies sold per hour.
+function randomNumGen (minCust, maxCust, cookies) {
+  var people = Math.floor(Math.random() * (maxCust - minCust)) + minCust;
+  return people * cookies;
+}
+
+function listStore() {
+  var list = document.createElement('ul'); // two arrays to creat strings to be inputed into html
+  var listArr = [];
+  var timeArr = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+}
+for (var i=0; i < 15; i++) {
+}
+}
+
+
 
 // Declare function that gives a random number between min and max customers. Multiply RandomCustNum and average cookies per sale.
 
-
-// function storeSales(store) {
-//   var list = document.createElement('ul');
-//   var listArr = [];
-//   var timeArr = ['0600', '0700', '0800', '0900', '1000', '1100', '1200', '1300', '1400', '1500', '1600', '1700', '1800', '1900', '2000'];
 //
 //   for (var i=0; i < 15; i++) {
 //     var numCookies = CookiesSoldHr(store.minCust, store.maxCust, store.avgSale);
