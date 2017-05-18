66fc62eb8cb31ececd718acf170222085845ea2a
George Embry <georgetembry@yahoo.com>
2017-05-18 16:08:52 -0700

    added randomNumGen to display

--- app.js
+++ app.js
@@ -16,9 +16,7 @@ function buildHeader(){
   console.log('header', header);
   // Created a table-row element and assigned it as the value of hoursRow variable
   var hoursRow = document.createElement('tr');
-  console.log('hoursRow', hoursRow);
-  // append hoursRow to header as a child element
-  header.appendChild(hoursRow);
+  console.log('hoursRow', hoursRow);  header.appendChild(hoursRow);
 // Use a for loop to itirate through the hours array in order to create and append td elements to hoursRow showing business hours
   for(var i = 0; i < hours.length; i++) {
     // create td element for each index of hours array in order to append them to table row
@@ -31,51 +29,39 @@ function buildHeader(){
   }
 
 }
-
 buildHeader();
 
 // renderHeader();
-function renderTable(){
+Store.prototype.renderTable = function() {
   var table = document.getElementById('shell');
   console.log('table', table);
   var data = [];
 
-  for (var i = 0; i < stores.length; i++) {
+  for (var i = 0; i < hours.length; i++) {
     data.push(
-      '<td>' + stores[i] + '</td>'
-      // '<td>' + stores[i].minCust + '</td>' +
-      // '<td>' + stores[i].maxCust + '</td>' +
-      // '<td>' + stores[i].avgSale + '</td>'
+      '<td>' + randomNumGen(this.minCust, this.maxCust, this.avgSale) + '</td>'
     );
-
   }
-}
-renderTable();
+  var storeRow = document.createElement('tr');
+  console.log('storeRow', storeRow);
+  storeRow.innerHTML = data.join('');
+  table.appendChild(storeRow);
+};
+
 
 // function that generates random number of cookies sold per hour.
 function randomNumGen(minCust, maxCust, avgSale) {
   var people = Math.floor(Math.random() * (maxCust - minCust)) + minCust;
-  return people * avgSale;
+  return Math.floor(people * avgSale);
 }
-
-// function listStore(store) {
-//   var list = document.createElement('ul'); // two arrays to create strings to be inputed into html
-//   var listArr = [];
-//   var cookieArr = []; // stores number of cookies sold per hour so it can be totaled
-//   // displays business hours alongside cookies sold each hour
-//   var hoursArr = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
-// }
-// listStore();
-//
-// for (var i = 0; i < 15; i++) {
-//   var thisHr = Math.floor(randomNumGen(store.minCust, store.maxCust, store.avgSale)); // a for loop that uses randomNumGen function to get number of cookies for each hour, then pushes each number into listArr and cookieArr
-//   console.log ('Cookies: ', thisHr);
-//   listArr.push ('<li>' + timeArr[i] + thisHr + '</li>');
-//   cookieArr.push(thisHr);
-// }
 
 var pike = new Store('1st and Pike', 23, 3, 6.3);
 var seatac = new Store('Seatac Airport', 3, 24, 1.2);
 var sCenter = new Store('Seattle Center', 11, 38, 3.7);
 var capHill = new Store('Capitol Hill', 20, 38, 2.3);
 var alki = new Store('Alki', 2, 16, 4.6);
+pike.renderTable();
+seatac.renderTable();
+sCenter.renderTable();
+capHill.renderTable();
+alki.renderTable();
